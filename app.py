db3fbdb642dc515a34870ac4262535efe0694c68
madhanselvaraj269 <45449958+madhanselvaraj269@users.noreply.github.com>
2025-02-22 16:09:48 +0530

    Update app.py as updated with small changes

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # cook your dish here
 
-print("Hello Madhan Welcome to the git")
-print("Hello Madhan Welcome to the git")
-print("Hello Madhan Welcome to the git")
+print("Hello Madhan Welcome to the git1")
+print("Hello cena Welcome to the git")
+print("Hello selvaraj Welcome to the git")
 print("Hello Madhan Welcome to the git")
 print("Hello Madhan Welcome to the git")
